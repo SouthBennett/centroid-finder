ed5480f0ad303cfa2bb7a893c58d6117e8e1a9ec
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-05-25 18:01:11 -0700

    Fully implemented List Avaliable Videos endpoint

--- server/routes/videoRoutes.js
+++ server/routes/videoRoutes.js
@@ -1,9 +1,19 @@
 import express from "express";
+import fs from "fs";
 
 const router = express.Router();
 
 router.get("/videos", (req, res) => {
-  res.send("Video route works!")
+  // res.send("Video route works!")
+  try {
+    const videos = fs.readdirSync("./videos");
+    res.status(200).json(videos);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "Error reading video directory"
+    });
+  }
 });
 
 export default router;
