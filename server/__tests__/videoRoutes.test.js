a09ee42f29779c002cbb9f94ba813c7cc241d910
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-05-27 16:26:56 -0700

    Configured Jest for testing and added first endpoint test /api/videos

---
+++ server/__tests__/videoRoutes.test.js
@@ -0,0 +1,19 @@
+import request from "supertest"; // import Supertest to simualte HTTP requests
+import express from "express"; // import express to create a small test server
+import videoRoutes from "../routes/videoRoutes.js"; //import the real video routes from the project
+
+const app = express(); // creates instance of express 
+
+app.use(express.json()) // allow json request bodies
+
+app.use("/api", videoRoutes); // mount routes under /api
+
+// Test the GET /api/videos endpoint
+test("GET /api/videos returns a list of videos", async () => {
+  // Send a fake GET request to the endpoint
+  const response = await request(app).get("/api/videos");
+  //Check that the endpoint returns a status 200
+  expect(response.statusCode).toBe(200);
+  // Check that the response body is an array
+  expect(Array.isArray(response.body)).toBe(true);
+})
